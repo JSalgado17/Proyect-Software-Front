b307a48f4b7caf5387b2fd56cc2bfa48acc1a80f
Jorge Salgado <68752578+JSalgado17@users.noreply.github.com>
2022-03-20 05:15:31 -0600

    Actualizado

--- Fronted/src/App.js
+++ Fronted/src/App.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import { Provider } from 'react-redux';
-=======
->>>>>>> 6474c9b60b2ea44f58c790400e3015b2598fdfe3
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppRouter from  './routers/AppRouter';
@@ -12,13 +9,9 @@ import Signin from './Components/Views/SigninPage';
 
 function App() {
   return (
-<<<<<<< HEAD
     <Provider store={store}>
       <Signin></Signin>
     </Provider>
-=======
-  <></>
->>>>>>> 6474c9b60b2ea44f58c790400e3015b2598fdfe3
   );
 }
 
