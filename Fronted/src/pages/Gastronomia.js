18968b47973ad7640ebbb2beb398bd9d9b7a6064
graciayanez <79233850+graciayanez@users.noreply.github.com>
2022-03-28 18:10:09 -0600

    MENSAJE

--- Fronted/src/pages/Gastronomia.js
+++ Fronted/src/pages/Gastronomia.js
@@ -3,10 +3,66 @@ import Footer from '../Componentes/layouts/Footer'
 const Gastronomia = () => {
     return (
         <body>
-<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
-            <h1>Gastronomia</h1>
-        </div>
+<div style={{ display: 'flex', flexDirection:"column", height: '60%' }}>
+                <div style={{height:"50%",width:"100%"}}>
+                    <img src='https://thumbs.dreamstime.com/b/comida-mexicana-doble-borde-vista-superior-sobre-fondo-de-banner-madera-oscura-con-espacio-copia-frontera-el-un-cartel-tacos-191565020.jpg' width={"100%"} height={"100%"}/>
+                </div>
 
+                <div style={{height:"50%",width:"100%"}}>
+                <h1 style={{textAlign:"center"}}>Platillos</h1>
+                </div>
+
+                <div style={{display:'flex',flexWrap:"wrap", height:"50%", width:"100%"}}>
+                    <div style={{width:"45%",flexDirection:"column", margin:"1%"}}>
+                        <h5 style={{textAlign:"center"}}>Aperitivos</h5>
+                        <text style={{textAlign:"center"}}>Anafre_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.100</text><br/>
+                        <text style={{textAlign:"center"}}>Nachos de Pollo _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.150</text><br/>
+                        <text style={{textAlign:"center"}}>Crema de Brocoli_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.80</text><br/>
+                        <text style={{textAlign:"center"}}>Crema de Esparrago_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.80</text><br/>
+                    </div>
+                <div style={{width:"45%",flexDirection:"column", margin:"1%"}}>
+                    <h5 style={{textAlign:"center"}}>Ensaladas</h5>
+                        <text style={{textAlign:"center"}}> Ensalada de Camarones _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.220</text><br/>
+                        <text style={{textAlign:"center"}}>Ensalada de Pollo_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.140</text><br/>
+                        <text style={{textAlign:"center"}}>Ensalada Mixta(Res y Pollo)_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.160</text><br/>
+                        <text style={{textAlign:"center"}}>Ensalada Cesar de Pollo_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.140</text><br/>
+                    </div>
+
+                    <div style={{display:'flex',flexWrap:"wrap", height:"50%", width:"100%"}}>
+                    <div style={{width:"45%",flexDirection:"column", margin:"1%"}}>
+                        <h5 style={{textAlign:"center"}}>Cocteles</h5>
+                        <text style={{textAlign:"center"}}>Coctel de Camarones _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.255</text><br/>
+                        <text style={{textAlign:"center"}}>Coctel de Camaron en Salsa Rosada _ _ _ _ _ _ _ _ _ _ _ L.245</text><br/>
+                        <text style={{textAlign:"center"}}>Ceviche de Camaron_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.210</text><br/>
+                        <text style={{textAlign:"center"}}>Crema de Esparrago_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.250</text><br/>
+                    </div>
+                <div style={{width:"45%",flexDirection:"column", margin:"1%"}}>
+                    <h5 style={{textAlign:"center"}}>Pastas</h5>
+                        <text style={{textAlign:"center"}}>Pasta Alfredo_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.230</text><br/>
+                        <text style={{textAlign:"center"}}>Spaguetty al Olio_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.175</text><br/>
+                        <text style={{textAlign:"center"}}>Pasta Primavera_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.150</text><br/>
+                        <text style={{textAlign:"center"}}>Pasta al Pesto_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.190</text><br/>
+                    </div>
+
+                    <div style={{display:'flex',flexWrap:"wrap", height:"50%", width:"100%"}}>
+                    <div style={{width:"45%",flexDirection:"column", margin:"1%"}}>
+                        <h5 style={{textAlign:"center"}}>Platos Mixtos</h5>
+                        <text style={{textAlign:"center"}}>Parrillada Paraiso (6 personas) _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.1,480</text><br/>
+                        <text style={{textAlign:"center"}}>Parrillada Especial (6 personas) _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.950</text><br/>
+                        <text style={{textAlign:"center"}}>Parrillada (3 personas) _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.700</text><br/>
+                        <text style={{textAlign:"center"}}>Plato Tipico_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.185</text><br/>
+                    </div>
+                <div style={{width:"45%",flexDirection:"column", margin:"1%"}}>
+                    <h5 style={{textAlign:"center"}}>Carnes</h5>
+                        <text style={{textAlign:"center"}}>Filete Tornado (Res y Camaron)_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.370</text><br/>
+                        <text style={{textAlign:"center"}}>Filete Mignon_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.325</text><br/>
+                        <text style={{textAlign:"center"}}>Puyaso _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _L.320</text><br/>
+                        <text style={{textAlign:"center"}}>Filete a la pimienta _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ L.140</text><br/>
+                    </div>
+                </div>
+                </div>
+                </div>
+                </div>
 <Footer />
 </body>
     )
