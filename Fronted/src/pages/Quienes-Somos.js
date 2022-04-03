14ab887c3b61ed2528828f0d72fe68a77b6d355d
dleann <dinaleanng@gmail.com>
2022-04-03 10:13:58 -0500

    Arreglo de diseño QuienesSomos(terminada)

--- Fronted/src/pages/Quienes-Somos.js
+++ Fronted/src/pages/Quienes-Somos.js
@@ -11,9 +11,9 @@ const Quienes_Somos = () => {
             <body>
                 
                 <div style={{ display: 'flex',flexWrap:"wrap", height: '50vh',}}>
-                    <div style={{display:"flex",justifyContent:"center",flexDirection:"column",alignContent:"center",width:"70%",height:"100%", marginLeft:"4%"}}>
+                    <div style={{display:"flex",justifyContent:"center",flexDirection:"column",alignContent:"center",width:"60%",height:"100%", marginLeft:"7%", marginTop:"2%"}}>
                         <h2 style={{textAlign:"center"}}>¿Quien Somos?</h2>
-                        <text style={{textAlign:"center", margin:"2%",backgroundColor: 'rgba(51, 51, 51, 0.5)', borderRadius: 10, fontSize:"25"}}>
+                        <text style={{textAlign:"center", margin:"2%",backgroundColor: 'rgba(49, 49, 49, 0.4)', borderRadius: 10, fontSize:"105%"}}>
                         Nuestra cadena de hoteles es una Cadena Hotelera fundada en 1992 como continuación de otra organización turística y hotelera nacida en 1975, dirigidas ambas por las mismas personas, dedicada al Turismo, la Hostelería y la Restauración.Nuestra filosofía se basa en ofrecer una excelente relación calidad-precio en los servicios, siguiendo criterios de estricta profesionalidad.
 El confort y la calidad de servicio están asegurados en todos nuestros hoteles ya que desde sus inicios, la empresa, se ha comprometido con la calidad en todas sus variables, ofreciendo la mejor y más digna posible a sus clientes, preparando y cuidando al personal más eficiente y profesional, manteniendo las instalaciones al nivel que el cliente espera encontrar en un hotel e innovando permanentemente en todos los niveles posibles de la escala de servicios que se ofrecen.
 
